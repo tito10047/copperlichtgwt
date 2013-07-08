1042b7aa1c04ec16e9d267d894740f336515fee0
mostka.j@gmail.com <mostka.j@gmail.com@f40711ea-83bc-e65b-bec8-b7c264f6db73>
2013-07-08 18:02:45 +0000

    exported example to extra directory. make wraper as package

--- src/com/mostka/cl3d/server/werecars/Internal.java
+++
@@ -1,8 +0,0 @@
-package com.mostka.cl3d.server.werecars;
-
-public class Internal {
-
-	public int sangle=0;
-	public double speed=0;
-	public double zSpeed=0;
-}
--- src/com/mostka/cl3d/shared/werecars/message/UserId.java
+++
@@ -1,7 +0,0 @@
-package com.mostka.cl3d.shared.werecars.message;
-
-
-public interface UserId extends Type{
-	String getId();
-	void setId(String id);
-}
